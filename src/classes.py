9b51b9436c02947090891f5569469ed1f58e077f
Zotov Stanislav <zotovstanislav@mail.ru>
2024-02-13 23:44:36 +0300

    initial

---
+++ src/classes.py
@@ -0,0 +1,25 @@
+class Category():
+	name: str
+	description: str
+	goods: list
+
+	category_counter = 0
+	unique_goods_counter = 0
+
+	def __init__(self, name, description, goods):
+		self.name = name
+		self.description = description
+		self.goods = goods
+
+
+class Product():
+	name: str
+	description: str
+	price: float
+	amt_in_stock: int
+
+	def __init__(self, name, description, price, amt_in_stock):
+		self.name = name
+		self.description = description
+		self.price = price
+		self.amt_in_stock = amt_in_stock

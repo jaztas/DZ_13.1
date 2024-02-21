25fbd3d3b6adad96511f19eebd3a41d03538cb33
Zotov Stanislav <zotovstanislav@mail.ru>
2024-02-21 23:52:27 +0300

    переделал

--- src/classes.py
+++ src/classes.py
@@ -1,30 +1,42 @@
-class Category():
+class Category:
     name: str
     description: str
-    __goods: list
+    goods: list
 
     category_counter = 0
     unique_goods_counter = 0
+    goods = []
 
     def __init__(self, name, description, goods):
         self.name = name
         self.description = description
-        self.__goods = []
+        self.__goods = goods
+        Category.category_counter += 1
+        Category.unique_goods_counter += len(set(goods))
 
-    @classmethod
-    def add_product(cls, product):
-        cls.__goods.append(product)
-        print(f"Продукт '{product.name}' добавлен в категорию '{cls.name}'.")
+    # def add_product(self, product: str):
+    # 	self.unique_goods_counter += 1
+    # 	if self.name == self.name:
+    # 		self.category_counter += 1
+    # 	self.__goods.append(product)
+    # 	print(f"Продукт '{product}' добавлен в категорию '{self.name}'.")
 
     @property
     def get_goods(self):
-        result = ""
-        for product in self.__goods:
-            result += f"{product.name}, {product.price} руб. Остаток: {product.amt_in_stock} шт.\n"
+        for prod in self.__goods:
+            result = print(f"{prod.name}, {prod.price} руб. Остаток: {prod.amt_in_stock} шт.")
         return result
 
+    @get_goods.setter
+    def get_goods(self, product):
+        self.__goods.append(product)
 
-class Product():
+    @property
+    def goods_list(self):
+        return self.__goods
+
+
+class Product:
     name: str
     description: str
     price: float
@@ -36,9 +48,46 @@ class Product():
         self.price = price
         self.amt_in_stock = amt_in_stock
 
-    @staticmethod
-    def create_product(name, description, price, amt_in_stock):
-        new_product = Product(name, description, price, amt_in_stock)
-        return new_product
+    @classmethod
+    def make_product(cls, name, description, price, amt_in_stock, goods):
+        for i in goods:
+            if name == i.name:
+                amt_in_stock += i.amt_in_stock
+                if price < i.price:
+                    price = i.price
+        return cls(name, description, price, amt_in_stock)
+
+    @property
+    def price(self):
+        return self.price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Цена введена некорректно!")
+        else:
+            self.price = value
+
+
+# phones = Category('Телефоны', 'Хорошие', [])
+# iphone = Product('iPhone', 'Стильный телефон', 150, 5)
+# android = Product('Xiaomi', 'Умный телефон', 75, 10)
+#
+# print(phones.name)
+# print(phones.description)
+# print(phones.goods)
+# print(phones.category_counter)
+# print(phones.unique_goods_counter)
+#
+# print(iphone.name)
+# print(iphone.price)
+# print(iphone.description)
+# print(iphone.amt_in_stock)
+#
+# # print(android.name)
+# print(android.price)
+# print(android.description)
+# print(android.amt_in_stock)
+
 
 

28965358ab1f5cfe2ecb6ce43c695edd753e2830
Zotov Stanislav <zotovstanislav@mail.ru>
2024-02-24 23:24:59 +0300

    сделал защищенным атрибут _price в классе Product

--- src/classes.py
+++ src/classes.py
@@ -1,73 +1,66 @@
 class Category:
-    name: str
-    description: str
-    goods: list
+	name: str
+	description: str
+	goods: list
 
-    category_counter = 0
-    unique_goods_counter = 0
-    goods = []
+	category_counter = 0
+	unique_goods_counter = 0
+	goods = []
 
-    def __init__(self, name, description, goods):
-        self.name = name
-        self.description = description
-        self.__goods = goods
-        Category.category_counter += 1
-        Category.unique_goods_counter += len(set(goods))
+	def __init__(self, name, description, goods):
+		self.name = name
+		self.description = description
+		self.__goods = goods
+		Category.category_counter += 1
+		Category.unique_goods_counter += len(set(goods))
 
-    # def add_product(self, product: str):
-    # 	self.unique_goods_counter += 1
-    # 	if self.name == self.name:
-    # 		self.category_counter += 1
-    # 	self.__goods.append(product)
-    # 	print(f"Продукт '{product}' добавлен в категорию '{self.name}'.")
+	@property
+	def get_goods(self):
+		result = ''
+		for prod in self.__goods:
+			result += f"{prod.name}, {prod.price} руб. Остаток: {prod.amt_in_stock} шт."
+		return print(result)
 
-    @property
-    def get_goods(self):
-        result = ''
-        for prod in self.__goods:
-            result += f"{prod.name}, {prod.price} руб. Остаток: {prod.amt_in_stock} шт."
-        return print(result)
+	@get_goods.setter
+	def get_goods(self, product):
+		self.__goods.append(product)
 
-    @get_goods.setter
-    def get_goods(self, product):
-        self.__goods.append(product)
-
-    @property
-    def goods_list(self):
-        return self.__goods
+	@property
+	def goods_list(self):
+		return self.__goods
 
 
 class Product:
-    name: str
-    description: str
-    price: float
-    amt_in_stock: int
+	name: str
+	description: str
+	price: float
+	amt_in_stock: int
 
-    def __init__(self, name, description, price, amt_in_stock):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.amt_in_stock = amt_in_stock
+	def __init__(self, name, description, price, amt_in_stock):
+		self.name = name
+		self.description = description
+		self._price = price
+		self.amt_in_stock = amt_in_stock
 
-    @classmethod
-    def make_product(cls, name, description, price, amt_in_stock, goods):
-        for i in goods:
-            if name == i.name:
-                amt_in_stock += i.amt_in_stock
-                if price < i.price:
-                    price = i.price
-        return cls(name, description, price, amt_in_stock)
+	@classmethod
+	def make_product(cls, name, description, price, amt_in_stock, goods):
+		for i in goods:
+			if name == i.name:
+				amt_in_stock += i.amt_in_stock
+				if price < i.price:
+					price = i.price
+		return cls(name, description, price, amt_in_stock)
 
-    @property
-    def price(self):
-        return self.price
+	@property
+	def price(self):
+		return self._price
 
-    @price.setter
-    def price(self, value):
-        if value <= 0:
-            print("Цена введена некорректно!")
-        else:
-            self.price = value
+	@price.setter
+	def price(self, value):
+		if value <= 0:
+			print("Цена введена некорректно!")
+		else:
+			self._price = value
 
 
 # phones = Category('Телефоны', 'Хорошие', [])
@@ -82,13 +75,14 @@ class Product:
 #
 # print(iphone.name)
 # print(iphone.price)
+# iphone.price = 0
+# print(iphone.price)
+# iphone.price = 200
+# print(iphone.price)
 # print(iphone.description)
 # print(iphone.amt_in_stock)
 #
-# # print(android.name)
+# print(android.name)
 # print(android.price)
 # print(android.description)
 # print(android.amt_in_stock)
-
-
-
